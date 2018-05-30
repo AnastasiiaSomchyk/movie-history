c2f59c83818f988ad60f616d6abb55982faff589
Anastasiia Somchyk <e4nastya@gmail.com>
2018-05-30 10:20:50 -0500

    get movies fixed

--- javascripts/dom.js
+++ javascripts/dom.js
@@ -1,4 +1,4 @@
-const domString = (movieArray, config) => {
+const domString = (movieArray, config, wherToPrint) => {
   let strang = '';
   movieArray.forEach((movie, index) => {
     if (index % 3 === 0) {
@@ -6,9 +6,10 @@ const domString = (movieArray, config) => {
     }
     strang += `<div class="col-sm-6 col-md-4">`;
     strang += `<div class="thumbnail movie">`;
-    strang += `<img data-poster"${movie.poster_path}" src="${config.base_url}/w342${movie.poster_path}" alt="Movie Poster">`;
+    strang += `<img data-poster="${movie.poster_path}" src="${config.base_url}/w342${movie.poster_path}" alt="Movie Poster">`;
     strang += `<div class="caption">`;
-    strang += `<h3 class="movie-title">${movie.original_title}</h3>`;
+    /* movie.original_title ? movie.original_title : movie.title (if original title Returne original title if not return movie title) */
+    strang += `<h3 class="movie-title">${movie.original_title ? movie.original_title : movie.title}</h3>`;
     strang += `<p class="movie-overview">${movie.overview}</p>`;
     strang += `<p><a href="#" class="btn-red btn btn-primary" role="button">Review</a> <a href="#" class="btn btn-default addMovieToWishList" role="button">Wishlist</a></p>`;
     strang += `</div>`;
@@ -20,11 +21,11 @@ const domString = (movieArray, config) => {
 
   });
 
-  printToDom(strang);
+  printToDom(wherToPrint, strang);
 };
 
-const printToDom = (stringz) => {
-  $('#movies').html(stringz);
+const printToDom = (wherToPrint, stringz) => {
+  $(`#${wherToPrint}`).html(stringz);
 };
 
 module.exports = {
